0a8c4b34cbfee1d4387ecd6dcba28bcd74b1dbf7
Javid Rafiee <javid.rafiee@gmail.com>
2020-12-22 00:01:36 +0330

    Initial Project Add

---
+++ src/main/java/com/javid/springframework/sfgpetcliniceclipse/SfgPetClinicEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgpetcliniceclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicEclipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javid/springframework/sfgpetcliniceclipse/SfgPetClinicEclipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgpetcliniceclipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicEclipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
